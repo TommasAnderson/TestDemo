fc3da4b949d54bc698ed2edc5f1773626c3f4b14
XJ <xujian@neau.edu.cn>
2019-08-26 14:40:18 +0800

    testequals

---
+++ MyTest/src/cn/edu/neautest/TestEquals.java
@@ -0,0 +1,69 @@
+package cn.edu.neautest;
+/*
+ * 测试equals
+ * 
+ */
+public class TestEquals{
+	public static void main(String[] args) {
+				
+		User u1 = new User(1000,"高琪","123456");
+		User u2 = new User(1000,"高希希","123456");
+		
+		System.out.println(u1 == u2);  //不是同一个对象，所以返回false
+		System.out.println(u1.equals(u2));  // 重写方法后，id相同所以返回true
+		
+		
+		String str1 = new String("sxt");
+		String str2 = new String("sxt");
+		
+		System.out.println(str1 == str2);//不是同一个对象，返回false
+		System.out.println(str1.equals(str2));// 内容相同，返回true
+		
+				
+	}
+
+}
+
+/**
+ * @author Administrator
+ *
+ */
+class User {
+	int id;
+	String name;
+	String pwd;
+	public User(int id, String name, String pwd)
+	{
+		super();
+		this.id = id;
+		this.name = name;
+		this.pwd = pwd;
+	}
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+}
