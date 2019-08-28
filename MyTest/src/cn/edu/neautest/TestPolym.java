a65e7c9731e4264f618fedc9f9b733a6d32b239f
XJ <xujian@neau.edu.cn>
2019-08-28 15:49:10 +0800

    test Polym多态

---
+++ MyTest/src/cn/edu/neautest/TestPolym.java
@@ -0,0 +1,42 @@
+package cn.edu.neautest;
+
+/*
+ * 测试多态
+ * @Derek
+ */
+
+public class TestPolym{ //多态要有继承，重写
+	public static void main(String[] args){
+		Animal a = new Animal();
+		animalCry(a);
+		
+		Dog d = new Dog();
+		animalCry(d);
+		
+		Cat c = new Cat();
+		animalCry(c);
+	}
+	
+	static void animalCry(Animal a) { //父类应用指向子类对象，Animal属于父类，但传入的对象属于子类
+		a.shout();
+	}
+
+}
+
+class Animal{
+	public void shout() {
+		System.out.println("叫了一声！");
+	}
+}
+
+class Dog extends Animal {
+	public void shout() {
+		System.out.println("汪汪汪！");
+	}
+}
+
+class Cat extends Animal {
+	public void shout() {
+		System.out.println("喵喵喵喵");
+	}
+}
