a6ae303addd4581c76f2a6d522688e3316829ba0
XJ <xujian@neau.edu.cn>
2019-09-04 14:43:12 +0800

    数组循环赋值、打印

--- MyTest/src/cn/edu/neautest/TestArray.java
+++ MyTest/src/cn/edu/neautest/TestArray.java
@@ -3,9 +3,25 @@ package cn.edu.neautest;
 public class TestArray{
 	public static void main(String[] args)
 	{
-		int[] arr01;        //创建一个数组，也可以用 int arr01[] 这种形式
+		int[] arr00 ; //创建一个数组，但没有规定长度
+		int[] arr01 = new int[10];        //创建一个数组，也可以用 int arr01[] 这种形式
 		String[] arr02 = new String[5];  //创建一个数组，并指定长度
 		UserMan[] arr03 = new UserMan[3];     //创建一个对象数组
+		
+		arr01[0] = 13;
+		arr01[1] = 15;
+		//arr01[10] = 110; 数组下标从0至length-1，所以该数组下标超界 
+		
+		
+		//通过循环初始化数组
+		for (int i=0;i<arr01.length;i++) {
+			arr01[i] = 10*i;
+		}
+		
+		//通过循环打印数组
+		for (int i=0;i<arr01.length;i++) {
+			System.out.println(arr01[i]);
+		}
 	}
 
 }
